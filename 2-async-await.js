4adb195df2b9479b35e8241042bacf7b133faf68
Ganapatrao <33506967+ganapatrao@users.noreply.github.com>
2024-01-27 00:37:18 +0530

    asynch await

---
+++ 2-async-await.js
@@ -0,0 +1,35 @@
+const demo = new Promise((resolve, reject) => {
+    let test = true;
+  
+    setTimeout(() => {
+      if (test == true) {
+        console.log("downloaded");
+        resolve("promise resolve value");
+      } else {
+        reject("promise fail ho gaya");
+      }
+    }, 3000);
+  });
+  
+//   demo
+//     .then((result) => {
+//       console.log(" show downloaded notification");
+//       console.log(result);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+  
+
+
+
+// how to use async await
+
+async function getResult(){
+    let result = await demo // await infront promise
+    console.log(result)
+
+    console.log("hi")
+}
+
+getResult()
---
+++ 2-promise-all-race-any.js
@@ -0,0 +1,47 @@
+/*Promise all take array of promise and executes in parallel and returns reult after a time*/
+
+let car1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("car 1");
+    //reject("car 1 rejected");
+  }, 1000);
+});
+
+let car2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("car 2");
+  }, 2000);
+});
+//
+let car3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("car 3");
+  }, 3000);
+});
+
+// let car3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("rejected");
+//   }, 3000);
+// });
+
+//
+
+Promise.all([car1, car2, car3])
+  .then((result) => console.log("all success:",result))
+  .catch((error) => console.log("all error:",error));
+
+/*out put
+  [ 'car 1', 'car 2', 'car 3' ]
+  
+  if error it will just  show rejected in the above example
+  */
+
+Promise.any([car1, car2, car3])
+  .then((result) => console.log("any success:",result))
+  .catch((error) => console.log("any error",error));
+
+
+  Promise.race([car1, car2, car3])
+  .then((result) => console.log("race success:",result))
+  .catch((error) => console.log("race error",error));
